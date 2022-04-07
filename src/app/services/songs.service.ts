be5ad0ed57b5d7349327207afe8b0539b897cf35
Siwon <simonquachpro@gmail.com>
2022-04-08 00:24:33 +0200

    modifications paths pour rendre le proj utilisable pour toutes les machines en local

--- src/app/services/songs.service.ts
+++ src/app/services/songs.service.ts
@@ -3,31 +3,31 @@ export class Songs {
         {
           "id": "0",
           "name": "Do I Wanna Know",
-          "link": "http://127.0.0.1:8887/s1.mp3",
+          "link": "http://localhost:4200//src/assets/s1.mp3",
         },
     
         {
           "id": "1",
           "name": "Song 2",
-          "link": "http://127.0.0.1:8887/s2.mp3",
+          "link": "http://localhost:4200//src/assets/s2.mp3",
         },
     
         {
           "id": "2",
           "name":"Baiana",
-          "link": "http://127.0.0.1:8887/s3.mp3",
+          "link": "http://localhost:4200//src/assets/s3.mp3",
         },
     
         {
           "id": "3",
           "name":"Stressed Out",  
-          "link": "http://127.0.0.1:8887/s4.mp3",
+          "link": "http://localhost:4200//src/assets/s4.mp3",
         },
     
         {
           "id": "4",
           "name":"Dark Necessities",
-          "link": "http://127.0.0.1:8887/s5.mp3",
+          "link": "http://localhost:4200//src/assets/s5.mp3",
         }
       ];
 }
